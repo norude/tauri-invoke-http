1cb7d2b813c4a74907f2978e6d44482027f138cf
fedor <60283525+norude@users.noreply.github.com>
2024-05-26 19:44:40 +0300

    started moving to v2

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,28 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
-use std::{
-  collections::HashMap,
-  str::FromStr,
-  sync::{Arc, Mutex},
+use {
+  serde::Deserialize,
+  serde_json::Value as JsonValue,
+  std::{
+    collections::HashMap,
+    str::FromStr,
+    sync::{Arc, Mutex},
+  },
+  tauri::{
+    ipc::{CallbackFn, InvokeBody, InvokeResponder, InvokeResponse},
+    webview::InvokeRequest,
+    AppHandle, Manager, Runtime, Url,
+  },
+  tiny_http::{Header, Method, Request, Response},
 };
-
-use tauri::{
-  api::ipc::CallbackFn, AppHandle, InvokePayload, InvokeResponder, InvokeResponse, Manager, Runtime,
-};
-use tiny_http::{Header, Method, Request, Response};
-
+#[derive(Debug, Deserialize)]
+pub struct RecievedMessage {
+  pub cmd: String,
+  pub callback: CallbackFn,
+  pub error: CallbackFn,
+  pub payload: JsonValue,
+}
 fn cors<R: std::io::Read>(request: &Request, r: &mut Response<R>, allowed_origins: &[String]) {
   if allowed_origins.iter().any(|s| s == "*") {
     r.add_header(Header::from_str("Access-Control-Allow-Origin: *").unwrap());
@@ -30,7 +41,7 @@ fn cors<R: std::io::Read>(request: &Request, r: &mut Response<R>, allowed_origin
 pub struct Invoke {
   allowed_origins: Vec<String>,
   port: u16,
-  requests: Arc<Mutex<HashMap<usize, Request>>>,
+  requests: Arc<Mutex<HashMap<u32, Request>>>,
 }
 
 impl Invoke {
@@ -50,6 +61,8 @@ impl Invoke {
     let allowed_origins = self.allowed_origins.clone();
     std::thread::spawn(move || {
       for mut request in server.incoming_requests() {
+        let requests = requests.clone();
+        let allowed_origins = allowed_origins.clone();
         if request.method() == &Method::Options {
           let mut r = Response::empty(200u16);
           cors(&request, &mut r, &allowed_origins);
@@ -60,7 +73,7 @@ impl Invoke {
         let pieces = url.split('/').collect::<Vec<_>>();
         let window_label = pieces[1];
 
-        if let Some(window) = app.get_window(window_label) {
+        if let Some(window) = app.get_webview_window(window_label) {
           let content_type = request
             .headers()
             .iter()
@@ -68,16 +81,61 @@ impl Invoke {
             .map(|h| h.value.to_string())
             .unwrap_or_else(|| "application/json".into());
 
-          let payload: InvokePayload = if content_type == "application/json" {
+          let payload: InvokeRequest = if content_type == "application/json" {
             let mut content = String::new();
             request.as_reader().read_to_string(&mut content).unwrap();
-            serde_json::from_str(&content).unwrap()
+            let origin = request
+              .headers()
+              .iter()
+              .find(|h| h.field.equiv("Origin"))
+              .map(|h| h.value.to_string())
+              .expect("Invalid IPC request - No Origin");
+            let message: RecievedMessage = serde_json::from_str(&content).unwrap();
+            InvokeRequest {
+              cmd: message.cmd,
+              callback: message.callback,
+              error: message.error,
+              url: Url::parse(&origin).expect("invalid IPC request URL"),
+              body: InvokeBody::Json(message.payload),
+              headers: (&request
+                .headers()
+                .iter()
+                .map(|h| (h.field.to_string(), h.value.to_string()))
+                .collect::<HashMap<_, _>>())
+                .try_into()
+                .unwrap_or_default(),
+              invoke_key: format!("FIXME: {}:{}:", file!(), line!()), //FIXME
+            }
           } else {
             unimplemented!()
           };
           let req_key = payload.callback.0;
           requests.lock().unwrap().insert(req_key, request);
-          let _ = window.on_message(payload);
+          window.on_message(
+            payload,
+            Box::new(move |_webview, _cmd, response, callback, _error| {
+              let request = requests.lock().unwrap().remove(&callback.0).unwrap();
+              let response = match response {
+                InvokeResponse::Ok(r) => Ok(r),
+                InvokeResponse::Err(e) => Err(e),
+              };
+              let status: u16 = if response.is_ok() { 200 } else { 400 };
+
+              let mut r = match response {
+                Ok(tauri::ipc::InvokeBody::Json(r)) => {
+                  Response::from_string(serde_json::to_string(&r).unwrap())
+                }
+                Ok(tauri::ipc::InvokeBody::Raw(r)) => Response::from_data(r),
+                Err(tauri::ipc::InvokeError(e)) => {
+                  Response::from_string(serde_json::to_string(&e).unwrap())
+                }
+              }
+              .with_status_code(status);
+              cors(&request, &mut r, &allowed_origins);
+
+              request.respond(r).unwrap();
+            }),
+          );
         } else {
           let mut r = Response::empty(404u16);
           cors(&request, &mut r, &allowed_origins);
@@ -90,30 +148,34 @@ impl Invoke {
   pub fn responder<R: Runtime>(&self) -> Box<InvokeResponder<R>> {
     let requests = self.requests.clone();
     let allowed_origins = self.allowed_origins.clone();
-    let responder = move |_window, response: InvokeResponse, callback: CallbackFn, _error| {
+    Box::new(move |_webview, _cmd, response, callback, _error| {
       let request = requests.lock().unwrap().remove(&callback.0).unwrap();
-      let response = response.into_result();
+      let response = match response {
+        InvokeResponse::Ok(r) => Ok(r),
+        InvokeResponse::Err(e) => Err(e),
+      };
       let status: u16 = if response.is_ok() { 200 } else { 400 };
 
-      let mut r = Response::from_string(
-        serde_json::to_string(&match response {
-          Ok(r) => r,
-          Err(e) => e,
-        })
-        .unwrap(),
-      )
+      let mut r = match response {
+        Ok(tauri::ipc::InvokeBody::Json(r)) => {
+          Response::from_string(serde_json::to_string(&r).unwrap())
+        }
+        Ok(tauri::ipc::InvokeBody::Raw(r)) => Response::from_data(r.clone()),
+        Err(tauri::ipc::InvokeError(e)) => {
+          Response::from_string(serde_json::to_string(&e).unwrap())
+        }
+      }
       .with_status_code(status);
       cors(&request, &mut r, &allowed_origins);
 
       request.respond(r).unwrap();
-    };
-    Box::new(responder)
+    })
   }
 
   pub fn initialization_script(&self) -> String {
     format!(
       "
-        Object.defineProperty(window, '__TAURI_POST_MESSAGE__', {{
+        Object.defineProperty(__TAURI_INTERNALS__, 'postMessage', {{
           value: (message) => {{
             const request = new XMLHttpRequest();
             request.addEventListener('load', function () {{
@@ -127,7 +189,7 @@ impl Invoke {
               }}
               window[`_${{success ? message.callback : message.error}}`](arg)
             }})
-            request.open('POST', 'http://localhost:{}/' + window.__TAURI_METADATA__.__currentWindow.label, true)
+            request.open('POST', 'http://localhost:{}/' + window.__TAURI_INTERNALS__.metadata.currentWindow.label, true)
             request.setRequestHeader('Content-Type', 'application/json')
             request.send(JSON.stringify(message))
           }}
